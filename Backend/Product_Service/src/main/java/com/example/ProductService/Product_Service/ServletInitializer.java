6af78145d30a1c721e9a63de699ea1c4c28d0780
sampleanjan <chanivenianjankumarc@gmail.com>
2024-10-21 14:10:55 +0530

    set up

---
+++ Backend/Product_Service/src/main/java/com/example/ProductService/Product_Service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.ProductService.Product_Service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProductServiceApplication.class);
+	}
+
+}
